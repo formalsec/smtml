b649b8374f310b4871b6469c537da5f7348b7cb9
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-03-25 00:36:46 +0000

    feat: add common.mli and batch.mli ocamldoc

--- lib/batch.mli
+++ lib/batch.mli
@@ -8,13 +8,28 @@ and s = Solver.solver
 
 val time_solver : float ref
 val create : unit -> t
+
 val clone : t -> t
+(** [clone solver] makes a copy of the current [solver] *)
+
 val interrupt : unit -> unit
+(** [interrupt ()] sends interrupt signal to SMT solver *)
+
 val add : t -> Expression.t -> unit
+(** [add solver e] adds assertion [e] to [solver] *)
+
 val check : t -> Expression.t list -> bool
+(** [check solver [e1; ...; en]] checks the satisfiability of [e1, ..., en]
+    without adding the expressions as assertions to the solver *)
+
 val fork : t -> Expression.t -> bool * bool
+(** [fork solver e] checks the satisfiability of the fork on the condition [e] *)
+
 val model : t -> Model.model
+(** [model solver] get a model for the current state of the solver *)
+
 val get_model : t -> Model.model
+(** [get_model solver] get the last model produced by the solver *)
 
 val model_binds :
   Model.model -> (string * num_type) list -> (string * Num.t) list
---
+++ lib/common.mli
@@ -0,0 +1,8 @@
+open Base
+open Z3
+
+val ctx : Z3.context
+val encode_expr : ?bool_to_bv:bool -> Expression.t -> Expr.expr
+val encode_formula : Formula.t -> Expr.expr
+val int64_of_bv : Expr.expr -> Int64.t
+val int64_of_fp : Expr.expr -> int -> int -> Int64.t
