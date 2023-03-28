90971821a973d27df1484946a7471c1bd446e89e
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-03-28 20:46:04 +0100

    feat: logical operators for formulas

--- lib/formula.ml
+++ lib/formula.ml
@@ -10,6 +10,10 @@ type formula =
 
 type t = formula
 
+let ( ! ) (f : t) : t = Not f
+let ( && ) (f1 : t) (f2 : t) : t = And (f1, f2)
+let ( || ) (f1 : t) (f2 : t) : t = Or (f1, f2)
+
 let rec negate (f : formula) : formula =
   match f with
   | True -> False
