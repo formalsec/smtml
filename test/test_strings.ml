c0b299d121d656a12f75e6d600dc73ed49d2fd98
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-03-29 11:52:59 +0100

    refactor: use int relops in string tests

--- test/test_strings.ml
+++ test/test_strings.ml
@@ -15,12 +15,12 @@ let%test_unit _ = encode str_symb
 (* Satisfiability *)
 let%test _ =
   Batch.check solver
-    [ Relop (Str S.Eq, Unop (Str S.Len, str_symb), Unop (Str S.Len, str)) ]
+    [ Relop (Int I.Ge, Unop (Str S.Len, str_symb), Unop (Str S.Len, str)) ]
 
 let%test _ =
   not
     (Batch.check solver
        [
          Relop (Int I.Eq, Unop (Str S.Len, str_symb), Val (Int 4));
-         Relop (Str S.Eq, Unop (Str S.Len, str_symb), Unop (Str S.Len, str));
+         Relop (Int I.Eq, Unop (Str S.Len, str_symb), Unop (Str S.Len, str));
        ])
