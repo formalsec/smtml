d595b5f5c3134bbacf73c83baa0d3adf786da9f2
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-03-30 02:18:53 +0100

    test: unit tests for eval ad opt

---
+++ test/test_eval.ml
@@ -0,0 +1,19 @@
+open Base
+open Encoding
+open Types
+open Expression
+
+let solver = Batch.create ()
+let int_symb = Symbolic (`IntType, "x")
+
+(* Satisfiability *)
+let%test "eval_unsat" =
+  let unsat = Relop (Int I.Eq, Val (Int 0), Val (Int 1)) in
+  Option.is_none (Batch.eval solver int_symb [ unsat ])
+
+let%test "eval_unconstrained" = 
+  Option.is_some (Batch.eval solver int_symb [])
+
+let%test "eval_constrained" =
+  Caml.(=) (Option.some (Int 5))
+    (Batch.eval solver int_symb [ Relop (Int I.Eq, int_symb, Val (Int 5))])
