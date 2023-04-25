b5cbce94c6e383b2e971545b3438c564a94f988e
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-04-25 19:38:08 +0100

    test: f32 nan regression test

--- test/test_bools.ml
+++
@@ -1,9 +0,0 @@
-open Encoding
-
-let solver = Batch.create ()
-let int_symb = Expression.mk_symbol `IntType "x"
-let bool_symb = Expression.mk_symbol `BoolType "y"
-
-let%test "test_not" =
-  let pc = [ Boolean.mk_not (Boolean.mk_eq bool_symb (Boolean.mk_val true)) ] in
-  Some (Expression.Bool false) = Batch.eval solver bool_symb pc
--- test/test_eval.ml
+++ test/test_eval.ml
@@ -1,19 +1,19 @@
 open Encoding
 
 let solver = Batch.create ()
-let int_symb = Expression.mk_symbol `IntType "x"
-let bool_symb = Expression.mk_symbol `BoolType "y"
+let x = Expression.mk_symbol `IntType "x"
+let y = Expression.mk_symbol `BoolType "y"
 
 (* Satisfiability *)
 let%test "test_unsat" =
-  Option.is_none (Batch.eval solver int_symb [ Boolean.mk_val false ])
+  Option.is_none (Batch.eval solver x [ Boolean.mk_val false ])
 
-let%test "test_unconstrained" = Option.is_some (Batch.eval solver int_symb [])
+let%test "test_unconstrained" = Option.is_some (Batch.eval solver x [])
 
 let%test "test_constrained_int" =
   Some (Expression.Int 5)
-  = Batch.eval solver int_symb [ Integer.mk_eq int_symb (Integer.mk_val 5) ]
+  = Batch.eval solver x [ Integer.mk_eq x (Integer.mk_val 5) ]
 
 let%test "test_constrained_bool" =
-  let pc = [ Boolean.mk_eq bool_symb (Boolean.mk_val true) ] in
-  Some (Expression.Bool true) = Batch.eval solver bool_symb pc
+  let pc = [ Boolean.mk_eq y (Boolean.mk_val true) ] in
+  Some (Expression.Bool true) = Batch.eval solver y pc
