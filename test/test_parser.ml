209dded738b9052b009bdcbb69a319276b003ee0
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-05-30 18:23:28 +0100

    style: code formatting

--- test/test_parser.ml
+++ test/test_parser.ml
@@ -1,10 +1,11 @@
 open Core
 open Encoding
 
-let parse_and_run script = Run.parse_string script|> Eval.start
+let parse_and_run script = Run.parse_string script |> Eval.start
 
 let%expect_test _ =
-  let script = {|
+  let script =
+    {|
     (declare-fun x real)
     (declare-fun y real)
     (assert (real.eq y (real.mul x x))) 
