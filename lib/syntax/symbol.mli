ebdb6ecf4f30409d73af3f7c120ecfd4d1004d60
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-06-09 10:18:21 +0100

    feat: eval and symbol interfaces

---
+++ lib/syntax/symbol.mli
@@ -0,0 +1,10 @@
+open Core
+open Types
+
+type t [@@deriving compare, sexp_of, hash]
+
+val mk_symbol : expr_type -> String.t -> t
+val equal : t -> t -> Bool.t
+val rename : t -> String.t -> t
+val type_of : t -> expr_type
+val to_string : t -> String.t
