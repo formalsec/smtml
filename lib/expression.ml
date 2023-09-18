9366359e45d5adca83c3d4adefec2e3062bef9af
zapashcanon <leo@ndrs.fr>
2023-09-18 16:34:56 +0200

    fmt

--- lib/expression.ml
+++ lib/expression.ml
@@ -181,15 +181,15 @@ let rec type_of (e : expr) : expr_type =
 let negate_relop (e : expr) : expr =
   match e with
   | Relop (op, e1, e2) -> (
-      match op with
-      | Int op' -> Relop (Int (I.neg_relop op'), e1, e2)
-      | Real op' -> Relop (Real (R.neg_relop op'), e1, e2)
-      | Bool op' -> Relop (Bool (B.neg_relop op'), e1, e2)
-      | Str op' -> Relop (Str (S.neg_relop op'), e1, e2)
-      | I32 op' -> Relop (I32 (I32.neg_relop op'), e1, e2)
-      | I64 op' -> Relop (I64 (I64.neg_relop op'), e1, e2)
-      | F32 op' -> Relop (F32 (F32.neg_relop op'), e1, e2)
-      | F64 op' -> Relop (F64 (F64.neg_relop op'), e1, e2) )
+    match op with
+    | Int op' -> Relop (Int (I.neg_relop op'), e1, e2)
+    | Real op' -> Relop (Real (R.neg_relop op'), e1, e2)
+    | Bool op' -> Relop (Bool (B.neg_relop op'), e1, e2)
+    | Str op' -> Relop (Str (S.neg_relop op'), e1, e2)
+    | I32 op' -> Relop (I32 (I32.neg_relop op'), e1, e2)
+    | I64 op' -> Relop (I64 (I64.neg_relop op'), e1, e2)
+    | F32 op' -> Relop (F32 (F32.neg_relop op'), e1, e2)
+    | F64 op' -> Relop (F64 (F64.neg_relop op'), e1, e2) )
   | _ -> raise InvalidRelop
 
 let pp_unop fmt =
@@ -259,25 +259,19 @@ let pp_quantifier fmt = function
 let pp_vars fmt vars =
   Format.pp_print_list
     ~pp_sep:(fun fmt () -> Format.pp_print_string fmt ", ")
-    Symbol.pp
-    fmt vars
+    Symbol.pp fmt vars
 
 let rec pp fmt (e : expr) =
   let fprintf = Format.fprintf in
   match e with
   | Val v -> fprintf fmt "%a" Value.pp v
-  | SymPtr (base, offset) ->
-    fprintf fmt "(i32.add (i32 %ld) %a)" base pp offset
-  | Unop (op, e) ->
-    fprintf fmt "(%a %a)" pp_unop op pp e
-  | Binop (op, e1, e2) ->
-    fprintf fmt "(%a %a %a)" pp_binop op pp e1 pp e2
+  | SymPtr (base, offset) -> fprintf fmt "(i32.add (i32 %ld) %a)" base pp offset
+  | Unop (op, e) -> fprintf fmt "(%a %a)" pp_unop op pp e
+  | Binop (op, e1, e2) -> fprintf fmt "(%a %a %a)" pp_binop op pp e1 pp e2
   | Triop (op, e1, e2, e3) ->
     fprintf fmt "(%a %a %a %a)" pp_triop op pp e1 pp e2 pp e3
-  | Relop (op, e1, e2) ->
-    fprintf fmt "(%a %a %a)" pp_relop op pp e1 pp e2
-  | Cvtop (op, e) ->
-    fprintf fmt "(%a %a)" pp_cvtop op pp e
+  | Relop (op, e1, e2) -> fprintf fmt "(%a %a %a)" pp_relop op pp e1 pp e2
+  | Cvtop (op, e) -> fprintf fmt "(%a %a)" pp_cvtop op pp e
   | Symbol s -> fprintf fmt "%a" Symbol.pp s
   | Extract (e, h, l) -> fprintf fmt "(extract %a %d %d)" pp e l h
   | Concat (e1, e2) -> fprintf fmt "(++ %a %a)" pp e1 pp e2
@@ -368,176 +362,176 @@ let rec simplify ?(extract = true) (e : expr) : expr =
   | Val v -> Val v
   | SymPtr (base, offset) -> SymPtr (base, simplify offset)
   | Binop (I32 op, e1, e2) -> (
-      let e1' = simplify e1
-      and e2' = simplify e2 in
-      match (e1', e2') with
-      | SymPtr (b1, os1), SymPtr (b2, os2) -> (
-          match op with
-          | Sub when Int32.(b1 = b2) -> simplify (Binop (I32 Sub, os1, os2))
-          | _ -> Binop (I32 op, e1', e2') )
-      | SymPtr (base, offset), _ -> (
-          match op with
-          | Add ->
-            let new_offset = simplify (Binop (I32 Add, offset, e2')) in
-            simplify (SymPtr (base, new_offset))
-          | Sub ->
-            let new_offset = simplify (Binop (I32 Sub, offset, e2')) in
-            simplify (SymPtr (base, new_offset))
-          | _ -> Binop (I32 op, e1', e2') )
-      | _, SymPtr (base, offset) -> (
-          match op with
-          | Add ->
-            let new_offset = simplify (Binop (I32 Add, offset, e1')) in
-            simplify (SymPtr (base, new_offset))
-          | _ -> Binop (I32 op, e1', e2') )
-      | Val (Num (I32 0l)), _ -> (
-          match op with
-          | Add | Or | Sub -> e2'
-          | And | DivS | DivU | Mul | RemS | RemU -> Val (Num (I32 0l))
-          | _ -> Binop (I32 op, e1', e2') )
-      | _, Val (Num (I32 0l)) -> (
-          match op with
-          | Add | Or | Sub -> e1'
-          | And | Mul -> Val (Num (I32 0l))
-          | _ -> Binop (I32 op, e1', e2') )
-      | Val (Num n1), Val (Num n2) ->
-        Val (Num (Eval_numeric.eval_binop (I32 op) n1 n2))
-      | Binop (I32 op2, x, Val (Num v1)), Val (Num v2) when not (is_num x) -> (
-          match (op, op2) with
-          | Add, Add ->
-            let v = Eval_numeric.eval_binop (I32 Add) v1 v2 in
-            Binop (I32 Add, x, Val (Num v))
-          | Add, Sub | Sub, Add ->
-            let v = Eval_numeric.eval_binop (I32 Sub) v1 v2 in
-            Binop (I32 Add, x, Val (Num v))
-          | Sub, Sub ->
-            let v = Eval_numeric.eval_binop (I32 Add) v1 v2 in
-            Binop (I32 Sub, x, Val (Num v))
-          | _, _ -> Binop (I32 op, e1', e2') )
-      | (bop, Val (Num (I32 1l)) | Val (Num (I32 1l)), bop)
-        when is_relop bop && Poly.(op = And) ->
-        bop
+    let e1' = simplify e1
+    and e2' = simplify e2 in
+    match (e1', e2') with
+    | SymPtr (b1, os1), SymPtr (b2, os2) -> (
+      match op with
+      | Sub when Int32.(b1 = b2) -> simplify (Binop (I32 Sub, os1, os2))
       | _ -> Binop (I32 op, e1', e2') )
+    | SymPtr (base, offset), _ -> (
+      match op with
+      | Add ->
+        let new_offset = simplify (Binop (I32 Add, offset, e2')) in
+        simplify (SymPtr (base, new_offset))
+      | Sub ->
+        let new_offset = simplify (Binop (I32 Sub, offset, e2')) in
+        simplify (SymPtr (base, new_offset))
+      | _ -> Binop (I32 op, e1', e2') )
+    | _, SymPtr (base, offset) -> (
+      match op with
+      | Add ->
+        let new_offset = simplify (Binop (I32 Add, offset, e1')) in
+        simplify (SymPtr (base, new_offset))
+      | _ -> Binop (I32 op, e1', e2') )
+    | Val (Num (I32 0l)), _ -> (
+      match op with
+      | Add | Or | Sub -> e2'
+      | And | DivS | DivU | Mul | RemS | RemU -> Val (Num (I32 0l))
+      | _ -> Binop (I32 op, e1', e2') )
+    | _, Val (Num (I32 0l)) -> (
+      match op with
+      | Add | Or | Sub -> e1'
+      | And | Mul -> Val (Num (I32 0l))
+      | _ -> Binop (I32 op, e1', e2') )
+    | Val (Num n1), Val (Num n2) ->
+      Val (Num (Eval_numeric.eval_binop (I32 op) n1 n2))
+    | Binop (I32 op2, x, Val (Num v1)), Val (Num v2) when not (is_num x) -> (
+      match (op, op2) with
+      | Add, Add ->
+        let v = Eval_numeric.eval_binop (I32 Add) v1 v2 in
+        Binop (I32 Add, x, Val (Num v))
+      | Add, Sub | Sub, Add ->
+        let v = Eval_numeric.eval_binop (I32 Sub) v1 v2 in
+        Binop (I32 Add, x, Val (Num v))
+      | Sub, Sub ->
+        let v = Eval_numeric.eval_binop (I32 Add) v1 v2 in
+        Binop (I32 Sub, x, Val (Num v))
+      | _, _ -> Binop (I32 op, e1', e2') )
+    | (bop, Val (Num (I32 1l)) | Val (Num (I32 1l)), bop)
+      when is_relop bop && Poly.(op = And) ->
+      bop
+    | _ -> Binop (I32 op, e1', e2') )
   | Binop (I64 op, e1, e2) -> (
-      let e1' = simplify e1
-      and e2' = simplify e2 in
-      match (e1', e2') with
-      | SymPtr (b1, os1), SymPtr (b2, os2) -> (
-          match op with
-          | Sub when Int32.(b1 = b2) -> simplify (Binop (I64 Sub, os1, os2))
-          | _ -> Binop (I64 op, e1', e2') )
-      | SymPtr (base, offset), _ -> (
-          match op with
-          | Add ->
-            let new_offset = simplify (Binop (I64 Add, offset, e2')) in
-            simplify (SymPtr (base, new_offset))
-          | Sub ->
-            let new_offset = simplify (Binop (I64 Sub, offset, e2')) in
-            simplify (SymPtr (base, new_offset))
-          | _ -> Binop (I64 op, e1', e2') )
-      | _, SymPtr (base, offset) -> (
-          match op with
-          | Add ->
-            let new_offset = simplify (Binop (I64 Add, offset, e1')) in
-            simplify (SymPtr (base, new_offset))
-          | _ -> Binop (I64 op, e1', e2') )
-      | Val (Num (I64 0L)), _ -> (
-          match op with
-          | Add | Or | Sub -> e2'
-          | And | DivS | DivU | Mul | RemS | RemU -> Val (Num (I64 0L))
-          | _ -> Binop (I64 op, e1', e2') )
-      | _, Val (Num (I64 0L)) -> (
-          match op with
-          | Add | Or | Sub -> e1'
-          | And | Mul -> Val (Num (I64 0L))
-          | _ -> Binop (I64 op, e1', e2') )
-      | Val (Num v1), Val (Num v2) ->
-        Val (Num (Eval_numeric.eval_binop (I64 op) v1 v2))
-      | Binop (I64 op2, x, Val (Num v1)), Val (Num v2) when not (is_num x) -> (
-          match (op, op2) with
-          | Add, Add ->
-            let v = Eval_numeric.eval_binop (I64 Add) v1 v2 in
-            Binop (I64 Add, x, Val (Num v))
-          | Add, Sub | Sub, Add ->
-            let v = Eval_numeric.eval_binop (I64 Sub) v1 v2 in
-            Binop (I64 Add, x, Val (Num v))
-          | Sub, Sub ->
-            let v = Eval_numeric.eval_binop (I64 Add) v1 v2 in
-            Binop (I64 Sub, x, Val (Num v))
-          | _, _ -> Binop (I64 op, e1', e2') )
-      | (bop, Val (Num (I64 1L)) | Val (Num (I64 1L)), bop)
-        when is_relop bop && Poly.(op = And) ->
-        bop
+    let e1' = simplify e1
+    and e2' = simplify e2 in
+    match (e1', e2') with
+    | SymPtr (b1, os1), SymPtr (b2, os2) -> (
+      match op with
+      | Sub when Int32.(b1 = b2) -> simplify (Binop (I64 Sub, os1, os2))
       | _ -> Binop (I64 op, e1', e2') )
+    | SymPtr (base, offset), _ -> (
+      match op with
+      | Add ->
+        let new_offset = simplify (Binop (I64 Add, offset, e2')) in
+        simplify (SymPtr (base, new_offset))
+      | Sub ->
+        let new_offset = simplify (Binop (I64 Sub, offset, e2')) in
+        simplify (SymPtr (base, new_offset))
+      | _ -> Binop (I64 op, e1', e2') )
+    | _, SymPtr (base, offset) -> (
+      match op with
+      | Add ->
+        let new_offset = simplify (Binop (I64 Add, offset, e1')) in
+        simplify (SymPtr (base, new_offset))
+      | _ -> Binop (I64 op, e1', e2') )
+    | Val (Num (I64 0L)), _ -> (
+      match op with
+      | Add | Or | Sub -> e2'
+      | And | DivS | DivU | Mul | RemS | RemU -> Val (Num (I64 0L))
+      | _ -> Binop (I64 op, e1', e2') )
+    | _, Val (Num (I64 0L)) -> (
+      match op with
+      | Add | Or | Sub -> e1'
+      | And | Mul -> Val (Num (I64 0L))
+      | _ -> Binop (I64 op, e1', e2') )
+    | Val (Num v1), Val (Num v2) ->
+      Val (Num (Eval_numeric.eval_binop (I64 op) v1 v2))
+    | Binop (I64 op2, x, Val (Num v1)), Val (Num v2) when not (is_num x) -> (
+      match (op, op2) with
+      | Add, Add ->
+        let v = Eval_numeric.eval_binop (I64 Add) v1 v2 in
+        Binop (I64 Add, x, Val (Num v))
+      | Add, Sub | Sub, Add ->
+        let v = Eval_numeric.eval_binop (I64 Sub) v1 v2 in
+        Binop (I64 Add, x, Val (Num v))
+      | Sub, Sub ->
+        let v = Eval_numeric.eval_binop (I64 Add) v1 v2 in
+        Binop (I64 Sub, x, Val (Num v))
+      | _, _ -> Binop (I64 op, e1', e2') )
+    | (bop, Val (Num (I64 1L)) | Val (Num (I64 1L)), bop)
+      when is_relop bop && Poly.(op = And) ->
+      bop
+    | _ -> Binop (I64 op, e1', e2') )
   | Relop (I32 op, e1, e2) -> (
-      let e1' = simplify e1
-      and e2' = simplify e2 in
-      match (e1', e2') with
-      | Val (Num v1), Val (Num v2) ->
-        let ret = Eval_numeric.eval_relop (I32 op) v1 v2 in
-        Val (Num (Num.num_of_bool ret))
-      | SymPtr (_, _), Val (Num (I32 0l)) | Val (Num (I32 0l)), SymPtr (_, _) -> (
-          match op with
-          | Eq -> Val (Num (I32 0l))
-          | Ne -> Val (Num (I32 1l))
-          | _ -> Relop (I32 op, e1', e2') )
-      | SymPtr (b1, os1), SymPtr (b2, os2) -> (
-          let open Int32 in
-          match op with
-          | Eq when b1 = b2 -> Relop (I32 Eq, os1, os2)
-          | Eq when b1 <> b2 -> Val (Num (I32 0l))
-          | Ne when b1 = b2 -> Relop (I32 Ne, os1, os2)
-          | Ne when b1 <> b2 -> Val (Num (I32 1l))
-          | LtU when b1 = b2 -> Relop (I32 LtU, os1, os2)
-          | LeU when b1 = b2 -> Relop (I32 LeU, os1, os2)
-          | LtU -> Relop (I32 LtU, Val (Num (I32 b1)), Val (Num (I32 b2)))
-          | LeU -> Relop (I32 LeU, Val (Num (I32 b1)), Val (Num (I32 b2)))
-          | GtU when b1 = b2 -> Relop (I32 GtU, os1, os2)
-          | GeU when b1 = b2 -> Relop (I32 GeU, os1, os2)
-          | GtU -> Relop (I32 GtU, Val (Num (I32 b1)), Val (Num (I32 b2)))
-          | GeU -> Relop (I32 GeU, Val (Num (I32 b1)), Val (Num (I32 b2)))
-          | _ -> Relop (I32 op, e1', e2') )
+    let e1' = simplify e1
+    and e2' = simplify e2 in
+    match (e1', e2') with
+    | Val (Num v1), Val (Num v2) ->
+      let ret = Eval_numeric.eval_relop (I32 op) v1 v2 in
+      Val (Num (Num.num_of_bool ret))
+    | SymPtr (_, _), Val (Num (I32 0l)) | Val (Num (I32 0l)), SymPtr (_, _) -> (
+      match op with
+      | Eq -> Val (Num (I32 0l))
+      | Ne -> Val (Num (I32 1l))
       | _ -> Relop (I32 op, e1', e2') )
+    | SymPtr (b1, os1), SymPtr (b2, os2) -> (
+      let open Int32 in
+      match op with
+      | Eq when b1 = b2 -> Relop (I32 Eq, os1, os2)
+      | Eq when b1 <> b2 -> Val (Num (I32 0l))
+      | Ne when b1 = b2 -> Relop (I32 Ne, os1, os2)
+      | Ne when b1 <> b2 -> Val (Num (I32 1l))
+      | LtU when b1 = b2 -> Relop (I32 LtU, os1, os2)
+      | LeU when b1 = b2 -> Relop (I32 LeU, os1, os2)
+      | LtU -> Relop (I32 LtU, Val (Num (I32 b1)), Val (Num (I32 b2)))
+      | LeU -> Relop (I32 LeU, Val (Num (I32 b1)), Val (Num (I32 b2)))
+      | GtU when b1 = b2 -> Relop (I32 GtU, os1, os2)
+      | GeU when b1 = b2 -> Relop (I32 GeU, os1, os2)
+      | GtU -> Relop (I32 GtU, Val (Num (I32 b1)), Val (Num (I32 b2)))
+      | GeU -> Relop (I32 GeU, Val (Num (I32 b1)), Val (Num (I32 b2)))
+      | _ -> Relop (I32 op, e1', e2') )
+    | _ -> Relop (I32 op, e1', e2') )
   | Extract (_, _, _) when not extract -> e
   | Extract (s, h, l) when extract -> (
-      match s with
-      | Val (Num (I64 x)) ->
-        let x' = nland64 (Int64.shift_right x (l * 8)) (h - l) in
-        Val (Num (I64 x'))
-      | _ when h - l = size (type_of s) -> s
-      | _ -> e )
+    match s with
+    | Val (Num (I64 x)) ->
+      let x' = nland64 (Int64.shift_right x (l * 8)) (h - l) in
+      Val (Num (I64 x'))
+    | _ when h - l = size (type_of s) -> s
+    | _ -> e )
   | Concat (e1, e2) -> (
-      let e1' = simplify ~extract:false e1
-      and e2' = simplify ~extract:false e2 in
-      match (e1', e2') with
-      | Extract (Val (Num (I64 x2)), h2, l2), Extract (Val (Num (I64 x1)), h1, l1)
-        ->
-        let d1 = h1 - l1
-        and d2 = h2 - l2 in
-        let x1' = nland64 (Int64.shift_right x1 (l1 * 8)) d1
-        and x2' = nland64 (Int64.shift_right x2 (l2 * 8)) d2 in
-        let x = Int64.(shift_left x2' (Int.( * ) d1 8) lor x1') in
-        Extract (Val (Num (I64 x)), d1 + d2, 0)
-      | Extract (Val (Num (I32 x2)), h2, l2), Extract (Val (Num (I32 x1)), h1, l1)
-        ->
-        let d1 = h1 - l1
-        and d2 = h2 - l2 in
-        let x1' = nland32 (Int32.shift_right x1 (l1 * 8)) d1
-        and x2' = nland32 (Int32.shift_right x2 (l2 * 8)) d2 in
-        let x = Int32.(shift_left x2' (Int.( * ) d1 8) lor x1') in
-        Extract (Val (Num (I32 x)), d1 + d2, 0)
-      | Extract (s1, h, m1), Extract (s2, m2, l) when Poly.(s1 = s2) && m1 = m2 ->
-        Extract (s1, h, l)
-      | ( Extract (Val (Num (I64 x2)), h2, l2)
-        , Concat (Extract (Val (Num (I64 x1)), h1, l1), se) )
-        when not (is_num se) ->
-        let d1 = h1 - l1
-        and d2 = h2 - l2 in
-        let x1' = nland64 (Int64.shift_right x1 (l1 * 8)) d1
-        and x2' = nland64 (Int64.shift_right x2 (l2 * 8)) d2 in
-        let x = Int64.(shift_left x2' (Int.( * ) d1 8) lor x1') in
-        Extract (Val (Num (I64 x)), d1 + d2, 0) ++ se
-      | _ -> e1' ++ e2' )
+    let e1' = simplify ~extract:false e1
+    and e2' = simplify ~extract:false e2 in
+    match (e1', e2') with
+    | Extract (Val (Num (I64 x2)), h2, l2), Extract (Val (Num (I64 x1)), h1, l1)
+      ->
+      let d1 = h1 - l1
+      and d2 = h2 - l2 in
+      let x1' = nland64 (Int64.shift_right x1 (l1 * 8)) d1
+      and x2' = nland64 (Int64.shift_right x2 (l2 * 8)) d2 in
+      let x = Int64.(shift_left x2' (Int.( * ) d1 8) lor x1') in
+      Extract (Val (Num (I64 x)), d1 + d2, 0)
+    | Extract (Val (Num (I32 x2)), h2, l2), Extract (Val (Num (I32 x1)), h1, l1)
+      ->
+      let d1 = h1 - l1
+      and d2 = h2 - l2 in
+      let x1' = nland32 (Int32.shift_right x1 (l1 * 8)) d1
+      and x2' = nland32 (Int32.shift_right x2 (l2 * 8)) d2 in
+      let x = Int32.(shift_left x2' (Int.( * ) d1 8) lor x1') in
+      Extract (Val (Num (I32 x)), d1 + d2, 0)
+    | Extract (s1, h, m1), Extract (s2, m2, l) when Poly.(s1 = s2) && m1 = m2 ->
+      Extract (s1, h, l)
+    | ( Extract (Val (Num (I64 x2)), h2, l2)
+      , Concat (Extract (Val (Num (I64 x1)), h1, l1), se) )
+      when not (is_num se) ->
+      let d1 = h1 - l1
+      and d2 = h2 - l2 in
+      let x1' = nland64 (Int64.shift_right x1 (l1 * 8)) d1
+      and x2' = nland64 (Int64.shift_right x2 (l2 * 8)) d2 in
+      let x = Int64.(shift_left x2' (Int.( * ) d1 8) lor x1') in
+      Extract (Val (Num (I64 x)), d1 + d2, 0) ++ se
+    | _ -> e1' ++ e2' )
   | _ -> e
 
 let mk_relop ?(reduce : bool = true) (e : expr) (t : num_type) : expr =
