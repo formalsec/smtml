d88526aa009ca1a8474d43d2d6bed1f7a70f4a2d
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-09-27 18:10:46 +0100

    remove core from interpret

--- lib/interpret.ml
+++ lib/interpret.ml
@@ -1,5 +1,4 @@
 module Make (Solver : Solver_intf.S) = struct
-  open Core
   open Ast
 
   type config =
@@ -11,23 +10,24 @@ module Make (Solver : Solver_intf.S) = struct
 
   let eval (c : config) : config =
     let { code; smap; solver; pc } = c in
-    let i = List.hd_exn code in
+    let i = List.hd code in
     let code', pc' =
       match i with
       | Declare x ->
-        Hashtbl.add_exn smap ~key:(Symbol.to_string x) ~data:(Symbol.type_of x);
-        (List.tl_exn code, pc)
+        Hashtbl.add smap (Symbol.to_string x) (Symbol.type_of x);
+        (List.tl code, pc)
       | Assert e ->
         Solver.add solver [ e ];
-        (List.tl_exn code, e :: pc)
+        (List.tl code, e :: pc)
       | CheckSat ->
-        if Solver.check solver [] then printf "sat\n" else printf "unsat\n";
-        (List.tl_exn code, pc)
+        if Solver.check solver [] then Format.printf "sat\n"
+        else Format.printf "unsat\n";
+        (List.tl code, pc)
       | GetModel ->
         assert (Solver.check solver []);
         let model = Solver.model solver in
-        printf "%s" (Model.to_string (Option.value_exn model));
-        (List.tl_exn code, pc)
+        Format.printf "%s" (Model.to_string (Option.get model));
+        (List.tl code, pc)
     in
     { c with code = code'; pc = pc' }
 
@@ -37,7 +37,7 @@ module Make (Solver : Solver_intf.S) = struct
   let start (prog : Ast.t list) : unit =
     let c =
       { code = prog
-      ; smap = Hashtbl.create (module String)
+      ; smap = Hashtbl.create 0
       ; solver = Solver.create ()
       ; pc = []
       }
