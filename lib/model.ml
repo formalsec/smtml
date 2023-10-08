96e16ff91da626c7e8187213211e210e55a48f11
zapashcanon <leo@ndrs.fr>
2023-10-08 12:30:47 +0200

    add more pp functions

--- lib/model.ml
+++ lib/model.ml
@@ -10,12 +10,11 @@ let get_bindings (model : t) : (Symbol.t * Value.t) List.t =
 let evaluate (model : t) (symb : Symbol.t) : Value.t Option.t =
   Hashtbl.find model symb
 
-let to_string (model : t) : String.t =
-  let bindings =
-    Hashtbl.fold model ~init:"" ~f:(fun ~key ~data accum ->
-      let x = Symbol.to_string key
-      and t = Types.string_of_type (Symbol.type_of key)
-      and v = Value.to_string data in
-      sprintf "%s  (%s %s %s)\n" accum x t v )
-  in
-  sprintf "(model\n%s)" bindings
+let pp_bindings fmt model =
+  Hashtbl.iteri model ~f:(fun ~key ~data ->
+    let t = Symbol.type_of key in
+    Format.fprintf fmt "(%a %a %a)@\n" Symbol.pp key Types.pp_type t Value.pp
+      data )
+
+let pp fmt model = Format.fprintf fmt "(model@\n%a)" pp_bindings model
+let to_string (model : t) : String.t = Format.asprintf "%a" pp model
