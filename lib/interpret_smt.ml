409099c4a4063cf6389e74aba195fe8b0327efb0
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2023-12-26 18:32:55 +0000

    Interpret smt on simplified commands

--- lib/interpret_smt.ml
+++ lib/interpret_smt.ml
@@ -4,7 +4,7 @@ module Make (Solver : Solver_intf.S) = struct
   open Smtlib
 
   type exec_state =
-    { cmds : Smtlib.script
+    { cmds : (Expr.t, Ty.t) Smtlib.command list
     ; ty_env : (string, Ty.t) Hashtbl.t
     ; pc : Expr.t list
     ; solver : Solver.t
@@ -19,22 +19,18 @@ module Make (Solver : Solver_intf.S) = struct
     }
 
   let exec_cmd cmd (state : exec_state) : (exec_state, string) Result.t =
-    let { solver; pc; ty_env; _ } = state in
+    let { solver; pc; _ } = state in
     let st pc = Ok { state with pc } in
     match cmd with
-    | Assert _t ->
-      st pc
-      (* Solver.add solver [ e ]; *)
-      (* st (e :: pc) *)
+    | Assert t ->
+      Solver.add solver [ t ];
+      st (t :: pc)
     | Check_sat ->
       if Solver.check solver [] then Format.printf "sat\n"
       else Format.printf "unsat\n";
       st pc
     | Check_sat_assuming -> assert false
-    | Declare_const (sym, sort) ->
-      let* ty = Expr.Smtlib.to_type sort in
-      Hashtbl.add ty_env sym ty;
-      st pc
+    | Declare_const (_sym, _ty) -> st pc
     | Echo msg ->
       Format.printf "%s@\n" msg;
       st pc
@@ -53,7 +49,8 @@ module Make (Solver : Solver_intf.S) = struct
       let* state = exec_cmd cmd { state with cmds } in
       loop state
 
-  let main ?state (cmds : Smtlib.script) : (exec_state, string) Result.t =
+  let main ?state (cmds : (Expr.t, Ty.t) Smtlib.command list) :
+    (exec_state, string) Result.t =
     let st =
       match state with
       | None -> init_state cmds
