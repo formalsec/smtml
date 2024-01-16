02b20ea9cfc238e0d83343019de2c99a62c2789d
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-01-16 20:26:33 +0000

    Improve of bool

--- test/test_core.ml
+++ test/test_core.ml
@@ -3,14 +3,21 @@ open Ty
 open Expr
 module Solver = Solver.Batch (Z3_mappings)
 
-let symbol_x = Symbol.("x" @: Ty_bool)
-let x = mk_symbol symbol_x
+let ( => ) b1 b2 = Bool.((not b1) || b2)
 
 let () =
+  let symbol_x = Symbol.("x" @: Ty_bool) in
+  let x = mk_symbol symbol_x in
+  let y = mk_symbol Symbol.("y" @: Ty_bool) in
+  let z = mk_symbol Symbol.("z" @: Ty_bool) in
   let solver = Solver.create () in
-  assert (Solver.check solver Bool.[ x = v true ]);
+  assert (Solver.check solver Bool.[ x != v false ]);
   assert ({ e = Val True; ty = Ty_bool } = Solver.get_value solver x);
-  assert (Solver.check solver Bool.[ not (x = v true) ]);
+  Solver.add solver Bool.[ x => y && y => z ];
+  assert (Solver.check solver [ x => z ]);
+  let w = mk_symbol Symbol.("w" @: Ty_bool) in
+  let xor_xz = Binop (Xor, x, z) @: Ty_bool in
+  Solver.add solver Bool.[ w = Triop (Ite, xor_xz, v true, v false) @: Ty_bool ];
+  assert (Solver.check solver Bool.[ x && z => not w ]);
   let model = Solver.model solver in
-  let val_x = Option.bind model (fun m -> Model.evaluate m symbol_x) in
-  assert (Some Value.False = val_x)
+  Option.iter (Model.pp Format.std_formatter) model
