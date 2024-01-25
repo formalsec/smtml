b0e33bb7a35632a599a86cc7e125b87da9152270
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-01-25 00:13:25 +0000

    Fixes test

--- test/unit/test_normalization.ml
+++ test/unit/test_normalization.ml
@@ -6,6 +6,4 @@ let () =
   let y = Bitv.I32.sym "y" in
   let e1 = Bitv.I32.(x > y) in
   let e2 = Bitv.I32.(y < x) in
-  Format.printf "e1: %a@.e2: %a@." pp e1 pp e2;
-  (* Hash-consing not working?! *)
   assert (e1 == e2)
