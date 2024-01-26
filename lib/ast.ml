a731dfb43e1524b82648b9edd56bcfd22a28dc2d
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-01-26 18:32:30 +0000

    Improve Symbol's interface

--- lib/ast.ml
+++ lib/ast.ml
@@ -1,3 +1,5 @@
+open Format
+
 type t =
   | Assert of term
   | Check_sat
@@ -15,21 +17,20 @@ and binding = string * term
 let rec pp_term fmt = function
   | E e -> Expr.pp fmt e
   | Let (binds, t) ->
-    Format.fprintf fmt "(let (%a) %a)"
-      (Format.pp_print_list ~pp_sep:Format.pp_print_space pp_binding)
+    fprintf fmt "@[<v>(let@[<hov>@ (%a)@]@ %a)@]"
+      (pp_print_list ~pp_sep:pp_print_space pp_binding)
       binds pp_term t
 
-and pp_binding fmt (x, t) = Format.fprintf fmt "(%s %a)" x pp_term t
+and pp_binding fmt (x, t) = fprintf fmt "(%s %a)" x pp_term t
 
 let pp fmt (instr : t) =
   match instr with
-  | Assert t -> Format.fprintf fmt "(assert @[<h 2>%a@])" pp_term t
-  | Check_sat -> Format.pp_print_string fmt "(check-sat)"
-  | Push -> Format.pp_print_string fmt "(push)"
-  | Pop n -> Format.fprintf fmt "(pop %d)" n
+  | Assert t -> fprintf fmt "@[<hov 2>(assert@ %a)@]" pp_term t
+  | Check_sat -> pp_print_string fmt "(check-sat)"
+  | Push -> pp_print_string fmt "(push)"
+  | Pop n -> fprintf fmt "(pop %d)" n
   | Let_const s ->
-    let ty = Symbol.type_of s in
-    Format.fprintf fmt "(let-const %a %a)" Symbol.pp s Ty.pp ty
-  | Get_model -> Format.pp_print_string fmt "(get-model)"
+    fprintf fmt "(let-const %a %a)" Symbol.pp s Ty.pp (Symbol.ty s)
+  | Get_model -> pp_print_string fmt "(get-model)"
 
-let to_string (instr : t) : string = Format.asprintf "%a" pp instr
+let to_string (instr : t) : string = asprintf "%a" pp instr
--- lib/symbol.ml
+++ lib/symbol.ml
@@ -3,20 +3,19 @@ type t =
   ; name : String.t
   }
 
+let make (sort : Ty.t) (name : string) = { name; sort }
+
 let ( @: ) (name : string) (sort : Ty.t) = { name; sort }
-let mk_symbol (sort : Ty.t) (name : string) = name @: sort
 
 let equal (s1 : t) (s2 : t) : bool =
   s1.sort = s2.sort && String.equal s1.name s2.name
 
 let compare t1 t2 =
   let compare_name = compare t1.name t2.name in
-  if compare_name = 0 then
-    compare t1.sort t2.sort
-  else
-    compare_name
+  if compare_name = 0 then compare t1.sort t2.sort else compare_name
 
-let rename (s : t) (name : String.t) = { s with name }
-let type_of (s : t) : Ty.t = s.sort
+let ty (s : t) : Ty.t = s.sort
+
+let name (s : t) : string = s.name
+
 let pp fmt s = Format.pp_print_string fmt s.name
-let to_string (s : t) : string = s.name
