c89173fc25208f37848d7a6e10a8a7c36cba555c
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-01-26 23:39:15 +0000

    Refactor tests a little

--- test/unit/test_hash_consing.ml
+++ test/unit/test_hash_consing.ml
@@ -1,16 +1,17 @@
 open Encoding
 open Expr
+open Ty
 
 let () =
-  let module I32 = Expr.Bitv.I32 in
+  let module I32 = Bitv.I32 in
   assert (I32.sym "x" == I32.sym "x");
   assert (I32.sym "x" != I32.sym "y");
   let left_a = I32.sym "x" in
   let right_a = I32.sym "y" in
   let left_b = I32.sym "x" in
   let right_b = I32.sym "y" in
-  let a = Expr.Binop (Ty.Add, left_a, right_a) @: Ty.Ty_int in
-  let b = Expr.Binop (Ty.Add, left_b, right_b) @: Ty.Ty_int in
+  let a = Binop (Add, left_a, right_a) @: Ty_bitv S32 in
+  let b = Binop (Add, left_b, right_b) @: Ty_bitv S32 in
   assert (a == b);
   (*
      There should be only 3 elements in the hashcons table:
@@ -18,4 +19,4 @@ let () =
        2. y
        3. x + y
   *)
-  assert (Expr.H.length () == 3)
+  assert (Expr.Hc.length () = 3)
