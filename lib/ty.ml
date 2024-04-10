85fca8a14ca8a90725e72768496670713f358030
joaomhmpereira <joaompereira07@gmail.com>
2024-04-10 15:29:30 +0100

    add App value and type

--- lib/ty.ml
+++ lib/ty.ml
@@ -35,6 +35,7 @@ type t =
   | Ty_list
   | Ty_tuple
   | Ty_array
+  | Ty_app
 
 type unop =
   | Neg
@@ -268,6 +269,7 @@ let pp fmt = function
   | Ty_list -> pp_string fmt "list"
   | Ty_tuple -> pp_string fmt "tuple"
   | Ty_array -> pp_string fmt "array"
+  | Ty_app -> pp_string fmt "app"
 
 let pp_logic fmt : logic -> unit = function
   | AUFLIA -> pp_string fmt "AUFLIA"
@@ -310,4 +312,4 @@ let size (ty : t) : int =
   match ty with
   | Ty_bitv n | Ty_fp n -> n / 8
   | Ty_int | Ty_bool -> 4
-  | Ty_real | Ty_str | Ty_list | Ty_array | Ty_tuple -> assert false
+  | Ty_real | Ty_str | Ty_list | Ty_array | Ty_tuple | Ty_app -> assert false
