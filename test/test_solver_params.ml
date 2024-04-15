6a97b4b699b9772f1990237b37abbb30ebb2dcaa
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-04-16 00:14:14 +0100

    Start creating a testing library for the multiple solvers

---
+++ test/test_solver_params.ml
@@ -0,0 +1,19 @@
+open Encoding
+
+module Make (M : Mappings_intf.S) = struct
+  open Params
+  module Solver = Solver.Batch (M)
+
+  let () =
+    assert (default_value Timeout = Int32.(to_int max_int));
+    assert (default_value Model = true);
+    assert (default_value Unsat_core = false);
+    assert (default_value Ematching = true)
+
+  let () =
+    let params =
+      default () $ (Timeout, 900) $ (Unsat_core, true) $ (Model, false)
+      $ (Ematching, false)
+    in
+    ignore (Solver.create ~params ())
+end
---
+++ test/test_z3.ml
@@ -0,0 +1,4 @@
+open Encoding
+open Encoding_tests
+
+module Test_solver_params = Test_solver_params.Make (Z3_mappings)
