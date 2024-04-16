7a95da79efe518992182a0f0c4e35570689dab2b
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-04-16 17:44:29 +0100

    Adds testing harness and `test_lia`

---
+++ test/test_lia.ml
@@ -0,0 +1,14 @@
+open Encoding
+open Test_harness
+open Infix
+open Infix.Int
+
+module Make (M : Mappings_intf.S) = struct
+  module Solver = Solver.Incremental (M)
+
+  let () =
+    let solver = Solver.create ~logic:QF_LIA () in
+    let a = symbol "a" Ty_int in
+    Solver.add solver [ a + int 1 = int 2 => ((a * int 2) + int 2 = int 4) ];
+    assert_sat (Solver.check solver [])
+end
--- test/test_z3.ml
+++ test/test_z3.ml
@@ -1,3 +1,4 @@
 open Encoding
 open Encoding_tests
 module Test_solver_params = Test_solver_params.Make (Z3_mappings)
+module Test_lia = Test_lia.Make (Z3_mappings)
