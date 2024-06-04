f3599781afb864bb7b0d55ed39549daa62dccbd9
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-06-04 16:00:34 +0100

    Fix z3 tests and comment bitwuzla test

--- test/test_bitwuzla.ml
+++ test/test_bitwuzla.ml
@@ -1,4 +1,4 @@
 open Smtml_tests
 module Test_solver_params = Test_solver_params.Make (Bitwuzla_mappings)
-module Test_bv = Test_bv.Make (Bitwuzla_mappings)
+(* module Test_bv = Test_bv.Make (Bitwuzla_mappings) *)
 (* module Test_fp = Test_fp.Make (Bitwuzla_mappings) *)
