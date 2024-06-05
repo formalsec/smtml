3622cfe205bdf6de9f2b05ea1f3004ad0f815d70
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-06-05 13:54:45 +0200

    Adds `solver_type_of_string` function

--- lib/solver_dispatcher.ml
+++ lib/solver_dispatcher.ml
@@ -1,14 +1,20 @@
 (* TODO: put this in some other more appropriate module? *)
 type solver_type =
   | Z3_solver
-  | Z3_solver2
   | Cvc5_solver
   | Colibri2_solver
   | Bitwuzla_solver
 
 let mappings_of_solver : solver_type -> (module Mappings_intf.S) = function
   | Z3_solver -> (module Z3_mappings)
-  | Z3_solver2 -> (module Z3_mappings2)
   | Cvc5_solver -> (module Cvc5_mappings)
   | Colibri2_solver -> (module Colibri2_mappings)
   | Bitwuzla_solver -> (module Bitwuzla_mappings)
+
+let solver_type_of_string (s : string) : (solver_type, string) result =
+  match String.map Char.lowercase_ascii s with
+  | "z3" -> Ok Z3_solver
+  | "colibri2" -> Ok Colibri2_solver
+  | "bitwuzla" -> Ok Bitwuzla_solver
+  | "cvc5" -> Ok Cvc5_solver
+  | s -> Format.ksprintf Result.error "unknown solver %s" s
