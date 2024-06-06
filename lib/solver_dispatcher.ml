bbcf8e942c8399df186e01325395cdeaba592bc2
zapashcanon <leo@ndrs.fr>
2024-06-06 15:16:12 +0200

    change return type of `Solver_dispatcher.solver_type_of_string`, add `Solver_dispatcher.pp_solver_type` for easy use with cmdliner

--- lib/solver_dispatcher.ml
+++ lib/solver_dispatcher.ml
@@ -11,10 +11,17 @@ let mappings_of_solver : solver_type -> (module Mappings_intf.S) = function
   | Colibri2_solver -> (module Colibri2_mappings)
   | Bitwuzla_solver -> (module Bitwuzla_mappings)
 
-let solver_type_of_string (s : string) : (solver_type, string) result =
+let solver_type_of_string (s : string) :
+  (solver_type, [> `Msg of string ]) result =
   match String.map Char.lowercase_ascii s with
   | "z3" -> Ok Z3_solver
   | "colibri2" -> Ok Colibri2_solver
   | "bitwuzla" -> Ok Bitwuzla_solver
   | "cvc5" -> Ok Cvc5_solver
-  | s -> Format.ksprintf Result.error "unknown solver %s" s
+  | s -> Error (`Msg (Format.sprintf "unknown solver %s" s))
+
+let pp_solver_type fmt = function
+  | Z3_solver -> Format.fprintf fmt "Z3"
+  | Cvc5_solver -> Format.fprintf fmt "CVC5"
+  | Colibri2_solver -> Format.fprintf fmt "Colibri2"
+  | Bitwuzla_solver -> Format.fprintf fmt "Bitwuzla"
