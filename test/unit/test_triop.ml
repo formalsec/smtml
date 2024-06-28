5e75f56fae26cc2a352c0faa7cd08c231d088a97
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-06-28 12:46:17 +0200

    Test unop simplifications

--- test/unit/test_triop.ml
+++ test/unit/test_triop.ml
@@ -3,24 +3,31 @@ open Ty
 open Expr
 open Value
 
+let ( = ) = Expr.equal
+
+let int i = value (Int i)
+
+let str str = value (Str str)
+
 (* bool *)
 let () =
-  let i i = value (Int i) in
-  assert (triop Ty_bool Ite (value True) (i 1) (i 0) = i 1);
-  assert (triop Ty_bool Ite (value False) (i 1) (i 0) = i 0)
+  let ty = Ty_bool in
+  assert (Expr.equal (triop ty Ite (value True) (int 1) (int 0)) (int 1));
+  assert (Expr.equal (triop ty Ite (value False) (int 1) (int 0)) (int 0))
 
 (* str *)
 let () =
-  let i i = value (Int i) in
-  let v str = value (Str str) in
-  assert (triop Ty_str String_extract (v "abcd") (i 1) (i 2) = v "bc");
-  assert (triop Ty_str String_index (v "abcd") (v "bc") (i 0) = i 1);
-  assert (triop Ty_str String_replace (v "abcd") (v "bc") (v "ef") = v "aefd")
+  let ty = Ty_str in
+  assert (triop ty String_extract (str "abcd") (int 1) (int 2) = str "bc");
+  assert (triop ty String_index (str "abcd") (str "bc") (int 0) = int 1);
+  assert (
+    triop ty String_replace (str "abcd") (str "bc") (str "ef") = str "aefd" )
 
 (* list *)
 let () =
-  let i i = value (Int i) in
-  let v l = value (List l) in
+  let ty = Ty_list in
+  let list l = value (List l) in
   assert (
-    triop Ty_list List_set (v [ Int 0; Int 1; Int 2 ]) (i 1) (i 3)
-    = v [ Int 0; Int 3; Int 2 ] )
+    Expr.equal
+      (triop ty List_set (list [ Int 0; Int 1; Int 2 ]) (int 1) (int 3))
+      (list [ Int 0; Int 3; Int 2 ]) )
--- test/unit/test_unop.ml
+++ test/unit/test_unop.ml
@@ -3,70 +3,86 @@ open Ty
 open Expr
 open Value
 
+let ( = ) = Expr.equal
+
 (* int *)
 let () =
+  let ty = Ty_int in
   let v i = value (Int i) in
-  assert (unop Ty_int Neg (v 1) = v ~-1);
-  assert (unop Ty_int Abs (v ~-1) = v 1);
-  assert (unop Ty_int Neg (unop Ty_int Neg (v 1)) = v 1)
+  assert (unop ty Neg (v 1) = v ~-1);
+  assert (unop ty Abs (v ~-1) = v 1);
+  assert (
+    let x = symbol (Symbol.make ty "x") in
+    Expr.equal (unop ty Neg (unop ty Neg x)) x )
 
 (* real *)
 let () =
+  let ty = Ty_real in
   let v f = value (Real f) in
-  assert (unop Ty_real Neg (v 1.0) = v (-1.0));
-  assert (unop Ty_real Abs (v 1.0) = v 1.0);
-  assert (unop Ty_real Sqrt (v 4.0) = v 2.0);
-  assert (unop Ty_real Nearest (v 0.504) = v 1.0);
-  assert (unop Ty_real Ceil (v 0.3) = v 1.0);
-  assert (unop Ty_real Floor (v 0.7) = v 0.0);
-  assert (unop Ty_real Trunc (v 1.504) = v 1.0);
-  assert (unop Ty_real Is_nan (v Float.nan) = value True)
+  assert (unop ty Neg (v 1.0) = v (-1.0));
+  assert (unop ty Abs (v 1.0) = v 1.0);
+  assert (unop ty Sqrt (v 4.0) = v 2.0);
+  assert (unop ty Nearest (v 0.504) = v 1.0);
+  assert (unop ty Ceil (v 0.3) = v 1.0);
+  assert (unop ty Floor (v 0.7) = v 0.0);
+  assert (unop ty Trunc (v 1.504) = v 1.0);
+  assert (unop ty Is_nan (v Float.nan) = value True)
 
 (* string*)
 let () =
+  let ty = Ty_str in
   let v s = value (Str s) in
-  assert (unop Ty_str Length (v "abc") = value (Int 3));
-  assert (unop Ty_str Trim (v " abc\t\n") = value (Str "abc"))
+  assert (unop ty Length (v "abc") = value (Int 3));
+  assert (unop ty Trim (v " abc\t\n") = value (Str "abc"))
 
 (* bool *)
 let () =
-  let true_ = value True in
-  let false_ = value False in
-  assert (unop Ty_bool Not true_ = false_);
-  assert (unop Ty_bool Not (unop Ty_bool Not true_) = true_)
+  let ty = Ty_bool in
+  assert (unop ty Not Bool.true_ = Bool.false_);
+  assert (
+    let x = Expr.symbol (Symbol.make ty "x") in
+    Expr.equal (unop ty Not (unop ty Not x)) x )
 
 (* list *)
 let () =
+  let ty = Ty_list in
   let v l = value (List l) in
-  assert (unop Ty_list Head (v [ Int 1; Int 2; Int 3 ]) = value (Int 1));
+  let vlist = v [ Int 1; Int 2; Int 3 ] in
+  let x = Expr.symbol (Symbol.make Ty_int "x") in
+  let y = Expr.symbol (Symbol.make Ty_int "y") in
+  let slist = Expr.make (List [ x; y ]) in
+  assert (unop ty Head vlist = value (Int 1));
+  assert (Expr.equal (unop ty Tail vlist) (v [ Int 2; Int 3 ]));
+  assert (Expr.equal (unop ty Length vlist) (value (Int 3)));
   assert (
-    unop Ty_list Tail (v [ Int 1; Int 2; Int 3 ]) = value (List [ Int 2; Int 3 ]) );
-  assert (unop Ty_list Length (v [ Int 1; Int 2; Int 3 ]) = value (Int 3));
-  assert (
-    unop Ty_list Reverse (v [ Int 1; Int 2; Int 3 ])
-    = value (List [ Int 3; Int 2; Int 1 ]) );
-  assert (
-    unop Ty_list Reverse (unop Ty_list Reverse (v [ Int 1; Int 2; Int 3 ]))
-    = v [ Int 1; Int 2; Int 3 ] )
+    Expr.equal (unop ty Reverse vlist) (value (List [ Int 3; Int 2; Int 1 ])) );
+  assert (Expr.equal (unop ty Head slist) x);
+  assert (Expr.equal (unop ty Tail slist) (Expr.make (List [ y ])));
+  assert (Expr.equal (unop ty Length slist) (value (Int 2)));
+  assert (Expr.equal (unop ty Reverse (unop Ty_list Reverse slist)) slist)
 
 (* i32 *)
 let () =
+  let ty = Ty_bitv 32 in
   let v i = value (Num (I32 i)) in
-  assert (unop (Ty_bitv 32) Neg (v 1l) = v (-1l));
-  assert (unop (Ty_bitv 32) Not (v (-1l)) = v 0l)
+  assert (unop ty Neg (v 1l) = v (-1l));
+  assert (unop ty Not (v (-1l)) = v 0l)
 
 (* i64 *)
 let () =
+  let ty = Ty_bitv 64 in
   let v i = value (Num (I64 i)) in
-  assert (unop (Ty_bitv 64) Neg (v 1L) = v (-1L));
-  assert (unop (Ty_bitv 64) Not (v (-1L)) = v 0L)
+  assert (unop ty Neg (v 1L) = v (-1L));
+  assert (unop ty Not (v (-1L)) = v 0L)
 
 (* f32 *)
 let () =
+  let ty = Ty_fp 32 in
   let v f = value (Num (F32 (Int32.bits_of_float f))) in
-  assert (unop (Ty_fp 32) Trunc (v 0.75) = v 0.0)
+  assert (unop ty Trunc (v 0.75) = v 0.0)
 
 (* f64 *)
 let () =
+  let ty = Ty_fp 64 in
   let v f = value (Num (F64 (Int64.bits_of_float f))) in
-  assert (unop (Ty_fp 64) Trunc (v 0.75) = v 0.0)
+  assert (unop ty Trunc (v 0.75) = v 0.0)
