2bcdf4964e7c265067c5f20b0c1b5f93b22953ef
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2024-08-03 11:20:44 +0200

    Reorganise some stuff

--- test/test_bitwuzla.ml
+++
@@ -1,9 +0,0 @@
-open Smtml
-open Smtml_tests
-
-let () = assert Bitwuzla_mappings.is_available
-
-module Test_solver_params =
-  Test_solver_params.Make (Bitwuzla_mappings.Fresh.Make ())
-module Test_bv = Test_bv.Make (Bitwuzla_mappings)
-module Test_fp = Test_fp.Make (Bitwuzla_mappings)
