5cd18ba47b66f1af7908044a912cacbbec5a9339
João Pereira <joaompereira07@gmail.com>
2025-06-20 18:34:14 +0000

    Replace int2str and str2int with actual solver operators

--- src/smtml/dolmenexpr_to_expr.ml
+++ src/smtml/dolmenexpr_to_expr.ml
@@ -175,6 +175,10 @@ module DolmenIntf = struct
   module Int = struct
     include DTerm.Int
 
+    let to_string t = DTerm.String.of_int t
+
+    let of_string t = DTerm.String.to_int t
+
     let neg = DTerm.Int.minus
   end
 
