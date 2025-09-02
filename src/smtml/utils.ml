a4045215a3616a233a05362f54a3a7a8dbcffabd
felixL-K <floyaukahn@gmail.com>
2025-09-02 14:38:14 +0200

    pretty print without symbol number

--- src/smtml/utils.ml
+++ src/smtml/utils.ml
@@ -9,6 +9,37 @@ let run_and_time_call ~use f =
   use (stop -. start);
   result
 
+let rec pp_normalize fmt (hte : Expr.t) =
+  match Expr.view hte with
+  | Val v -> Value.pp fmt v
+  | Ptr { base; offset } -> Fmt.pf fmt "(Ptr %a %a)" Bitvector.pp base pp_normalize offset
+  | Symbol _ -> Fmt.pf fmt "symbol_"
+  | List v -> Fmt.pf fmt "@[<hov 1>[%a]@]" (Fmt.list ~sep:Fmt.comma pp_normalize) v
+  | App (s, v) ->
+    Fmt.pf fmt "@[<hov 1>(%a@ %a)@]" Symbol.pp s (Fmt.list ~sep:Fmt.comma pp_normalize) v
+  | Unop (ty, op, e) ->
+    Fmt.pf fmt "@[<hov 1>(%a.%a@ %a)@]" Ty.pp ty Ty.Unop.pp op pp_normalize e
+  | Binop (ty, op, e1, e2) ->
+    Fmt.pf fmt "@[<hov 1>(%a.%a@ %a@ %a)@]" Ty.pp ty Ty.Binop.pp op pp_normalize e1 pp_normalize e2
+  | Triop (ty, op, e1, e2, e3) ->
+    Fmt.pf fmt "@[<hov 1>(%a.%a@ %a@ %a@ %a)@]" Ty.pp ty Ty.Triop.pp op pp_normalize e1 pp_normalize
+      e2 pp_normalize e3
+  | Relop (ty, op, e1, e2) ->
+    Fmt.pf fmt "@[<hov 1>(%a.%a@ %a@ %a)@]" Ty.pp ty Ty.Relop.pp op pp_normalize e1 pp_normalize e2
+  | Cvtop (ty, op, e) ->
+    Fmt.pf fmt "@[<hov 1>(%a.%a@ %a)@]" Ty.pp ty Ty.Cvtop.pp op pp_normalize e
+  | Naryop (ty, op, es) ->
+    Fmt.pf fmt "@[<hov 1>(%a.%a@ (%a))@]" Ty.pp ty Ty.Naryop.pp op
+      (Fmt.list ~sep:Fmt.comma pp_normalize)
+      es
+  | Extract (e, h, l) -> Fmt.pf fmt "@[<hov 1>(extract@ %a@ %d@ %d)@]" pp_normalize e l h
+  | Concat (e1, e2) -> Fmt.pf fmt "@[<hov 1>(++@ %a@ %a)@]" pp_normalize e1 pp_normalize e2
+  | Binder (b, vars, e) ->
+    Fmt.pf fmt "@[<hov 1>(%a@ (%a)@ %a)@]" Binder.pp b (Fmt.list ~sep:Fmt.sp pp_normalize)
+      vars pp_normalize e
+
+let pp_list fmt (es : Expr.t list) = Fmt.list ~sep:Fmt.sp pp_normalize fmt es
+
 let write =
   let log_path : Fpath.t option =
     let env_var = "QUERY_LOG_PATH" in
