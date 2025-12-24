58421410352969a86879ba14a8b026a05229d6aa
Filipe Marques <filipe.s.marques@tecnico.ulisboa.pt>
2025-12-24 18:39:47 +0000

    Catch Eval_error exception when parsing files

--- src/bin/cmd_run.ml
+++ src/bin/cmd_run.ml
@@ -36,39 +36,48 @@ let run (s : Settings.Run.t) =
   let total_t = ref 0. in
   let exception_log = ref [] in
   let exception_count = ref 0 in
+
   let run_file state file =
     Log.debug (fun k -> k "File %a..." Fpath.pp file);
     incr total_tests;
     let start_t = Unix.gettimeofday () in
-    Fun.protect ~finally:(fun () ->
-      if s.print_statistics then (
-        let exec_t = Unix.gettimeofday () -. start_t in
-        total_t := !total_t +. exec_t;
-        Log.app (fun m -> m "Run %a in %.06f" Fpath.pp file exec_t) ) )
-    @@ fun () ->
     let ast =
-      try Ok (Compile.until_rewrite file)
-      with Parse.Syntax_error err -> Error (`Parsing_error (file, err))
+      try Ok (Compile.until_rewrite file) with
+      | Parse.Syntax_error err -> Error (`Parsing_error (file, err))
+      | Smtml.Eval.Eval_error err -> Error (`Eval_error err)
     in
-    match ast with
-    | Ok _ when s.dry -> state
-    | Ok ast ->
-      Some (Interpret.start ?state ast ~no_strict_status:s.no_strict_status)
-    | Error (`Parsing_error ((fpath, err_msg) as err)) ->
-      Log.err (fun k -> k "%a: %s" Fpath.pp fpath err_msg);
-      incr exception_count;
-      exception_log := err :: !exception_log;
-      state
+    let state =
+      match ast with
+      | Ok _ when s.dry -> state
+      | Ok ast ->
+        Some (Interpret.start ?state ast ~no_strict_status:s.no_strict_status)
+      | Error (`Parsing_error ((fpath, err_msg) as err)) ->
+        Log.err (fun k -> k "%a: %s" Fpath.pp fpath err_msg);
+        incr exception_count;
+        exception_log := err :: !exception_log;
+        state
+      | Error (`Eval_error kind) ->
+        Log.err (fun k ->
+          k "@[<h>%a: Eval_error: %a@]" Fpath.pp file Smtml.Eval.pp_error_kind
+            kind );
+        incr exception_count;
+        state
+    in
+    if s.print_statistics then (
+      let exec_t = Unix.gettimeofday () -. start_t in
+      total_t := !total_t +. exec_t;
+      Log.app (fun m -> m "Run %a in %.06f" Fpath.pp file exec_t) );
+    state
   in
+
   let run_dir prev_state d =
-    let result =
-      Bos.OS.Dir.fold_contents ~traverse:`Any
-        (fun path state ->
-          if Fpath.has_ext ".smt2" path then run_file state path else state )
-        prev_state d
-    in
-    match result with Error (`Msg e) -> failwith e | Ok state -> state
+    Bos.OS.Dir.fold_contents ~traverse:`Any
+      (fun path state ->
+        if Fpath.has_ext ".smt2" path then run_file state path else state )
+      prev_state d
+    |> Result.get_ok
   in
+
   let run_path prev_state path =
     match Fpath.to_string path with
     | "-" -> run_file prev_state path
@@ -84,6 +93,7 @@ let run (s : Settings.Run.t) =
         Log.err (fun k -> k "%s" err);
         prev_state )
   in
+
   let _ =
     match s.from_file with
     | None -> List.fold_left run_path None s.filenames
@@ -94,6 +104,7 @@ let run (s : Settings.Run.t) =
         None
       | Ok files -> List.fold_left run_file None files )
   in
+
   if s.print_statistics then Log.app (fun k -> k "total time: %.06f" !total_t);
   let write_exception_log =
     let open Smtml_prelude.Result in
