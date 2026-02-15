9955ec9dce31829c981ff41a815cbfa4572b289b
hra687261 <hra687261@gmail.com>
2026-02-15 16:43:51 +0100

    replace more floats with score

--- src/smtml/regression_model.ml
+++ src/smtml/regression_model.ml
@@ -7,18 +7,24 @@ module FeatMap = Feature_extraction.FeatMap
 
 type score = float
 
+let score_of_int = float_of_int
+
+let compare_score = Float.compare
+
+let to_score = to_float
+
 type tree =
-  | Leaf of float
+  | Leaf of score
   | Node of
       { feature : string
-      ; threshold : float
+      ; threshold : score
       ; left : tree
       ; right : tree
       }
 
 type gb_model =
   { (* n_estimators : int; *)
-    init_value : float
+    init_value : score
   ; trees : tree list
   }
 
@@ -31,11 +37,11 @@ type t =
 let rec tree_of_json json =
   match member "value" json with
   | `Float f -> Leaf f
-  | `Int i -> Leaf (float_of_int i)
+  | `Int i -> Leaf (score_of_int i)
   | `Null ->
     Node
       { feature = member "feature" json |> to_string
-      ; threshold = member "threshold" json |> to_float
+      ; threshold = member "threshold" json |> to_score
       ; left = member "left" json |> tree_of_json
       ; right = member "right" json |> tree_of_json
       }
@@ -47,7 +53,7 @@ let model_of_json json =
   if is_gb then
     GBModel
       { (* n_estimators = member "n_estimators" model_data |> to_int;  *)
-        init_value = member "init_value" model_data |> to_float
+        init_value = member "init_value" model_data |> to_score
       ; trees = member "trees" model_data |> to_list |> List.map tree_of_json
       }
   else DTModel (tree_of_json model_data)
@@ -61,12 +67,12 @@ let read_models_from_file filename =
 let rec eval_tree (feats : int FeatMap.t) = function
   | Leaf v -> v
   | Node { feature; threshold; left; right } ->
-    let value = float_of_int (FeatMap.find_def0 feature feats) in
-    if Float.compare value threshold <= 0 then eval_tree feats left
+    let value = score_of_int (FeatMap.find_def0 feature feats) in
+    if compare_score value threshold <= 0 then eval_tree feats left
     else eval_tree feats right
 
 let choose_best scores =
-  match List.sort (fun (a, _) (b, _) -> Float.compare a b) scores with
+  match List.sort (fun (a, _) (b, _) -> compare_score a b) scores with
   | [] | [ _ ] -> assert false
   | (_, hd) :: _ -> hd
 
