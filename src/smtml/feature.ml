eaa3cb8d0919a7d919ccc5b40c8a2dfee53d884f
Hichem R. A. <hra687261@gmail.com>
2026-06-09 01:22:39 +0200

    Use dedicated feat type

--- src/smtml/feature.ml
+++ src/smtml/feature.ml
@@ -1,185 +1,185 @@
-let of_unop (unop : Ty.Unop.t) : string =
+let of_unop (unop : Ty.Unop.t) : Feature_map.feat =
   match unop with
-  | Neg -> "Neg"
-  | Not -> "Not"
-  | Clz -> "Clz"
-  | Ctz -> "Ctz"
-  | Popcnt -> "Popcnt"
+  | Neg -> Neg
+  | Not -> Not
+  | Clz -> Clz
+  | Ctz -> Ctz
+  | Popcnt -> Popcnt
   (* Float *)
-  | Abs -> "Abs"
-  | Sqrt -> "Sqrt"
-  | Is_normal -> "Is_normal"
-  | Is_subnormal -> "Is_subnormal"
-  | Is_negative -> "Is_negative"
-  | Is_positive -> "Is_positive"
-  | Is_infinite -> "Is_infinite"
-  | Is_nan -> "Is_nan"
-  | Is_zero -> "Is_zero"
-  | Ceil -> "Ceil"
-  | Floor -> "Floor"
-  | Trunc -> "Trunc"
-  | Nearest -> "Nearest"
-  | Head -> "Head"
-  | Tail -> "Tail"
-  | Reverse -> "Reverse"
-  | Length -> "Length"
+  | Abs -> Abs
+  | Sqrt -> Sqrt
+  | Is_normal -> Is_normal
+  | Is_subnormal -> Is_subnormal
+  | Is_negative -> Is_negative
+  | Is_positive -> Is_positive
+  | Is_infinite -> Is_infinite
+  | Is_nan -> Is_nan
+  | Is_zero -> Is_zero
+  | Ceil -> Ceil
+  | Floor -> Floor
+  | Trunc -> Trunc
+  | Nearest -> Nearest
+  | Head -> Head
+  | Tail -> Tail
+  | Reverse -> Reverse
+  | Length -> Length
   (* String *)
-  | Trim -> "Trim"
+  | Trim -> Trim
   (* RegExp *)
-  | Regexp_star -> "Regexp_star"
-  | Regexp_loop _ -> "Regexp_loop"
-  | Regexp_plus -> "Regexp_plus"
-  | Regexp_opt -> "Regexp_opt"
-  | Regexp_comp -> "Regexp_comp"
+  | Regexp_star -> Regexp_star
+  | Regexp_loop _ -> Regexp_loop
+  | Regexp_plus -> Regexp_plus
+  | Regexp_opt -> Regexp_opt
+  | Regexp_comp -> Regexp_comp
 
-let of_binop (binop : Ty.Binop.t) : string =
+let of_binop (binop : Ty.Binop.t) : Feature_map.feat =
   match binop with
-  | Add -> "Add"
-  | Sub -> "Sub"
-  | Mul -> "Mul"
-  | Div -> "Div"
-  | DivU -> "DivU"
-  | Rem -> "Rem"
-  | RemU -> "RemU"
-  | Shl -> "Shl"
-  | ShrA -> "ShrA"
-  | ShrL -> "ShrL"
-  | And -> "And"
-  | Or -> "Or"
-  | Xor -> "Xor"
-  | Implies -> "Implies"
-  | Pow -> "Pow"
-  | Min -> "Min"
-  | Max -> "Max"
-  | Copysign -> "Copysign"
-  | Rotl -> "Rotl"
-  | Rotr -> "Rotr"
-  | At -> "At"
-  | List_cons -> "List_cons"
-  | List_append -> "List_append"
+  | Add -> Add
+  | Sub -> Sub
+  | Mul -> Mul
+  | Div -> Div
+  | DivU -> DivU
+  | Rem -> Rem
+  | RemU -> RemU
+  | Shl -> Shl
+  | ShrA -> ShrA
+  | ShrL -> ShrL
+  | And -> And
+  | Or -> Or
+  | Xor -> Xor
+  | Implies -> Implies
+  | Pow -> Pow
+  | Min -> Min
+  | Max -> Max
+  | Copysign -> Copysign
+  | Rotl -> Rotl
+  | Rotr -> Rotr
+  | At -> At
+  | List_cons -> List_cons
+  | List_append -> List_append
   (* String *)
-  | String_prefix -> "String_prefix"
-  | String_suffix -> "String_suffix"
-  | String_contains -> "String_contains"
-  | String_last_index -> "String_last_index"
-  | String_in_re -> "String_in_re"
+  | String_prefix -> String_prefix
+  | String_suffix -> String_suffix
+  | String_contains -> String_contains
+  | String_last_index -> String_last_index
+  | String_in_re -> String_in_re
   (* Regexp *)
-  | Regexp_range -> "Regexp_range"
-  | Regexp_inter -> "Regexp_inter"
-  | Regexp_diff -> "Regexp_diff"
+  | Regexp_range -> Regexp_range
+  | Regexp_inter -> Regexp_inter
+  | Regexp_diff -> Regexp_diff
 
-let of_triop (triop : Ty.Triop.t) : string =
+let of_triop (triop : Ty.Triop.t) : Feature_map.feat =
   match triop with
-  | Ite -> "Ite"
-  | List_set -> "List_set"
+  | Ite -> Ite
+  | List_set -> List_set
   (* String *)
-  | String_extract -> "String_extract"
-  | String_replace -> "String_replace"
-  | String_index -> "String_index"
-  | String_replace_all -> "String_replace_all"
-  | String_replace_re -> "String_replace_re"
-  | String_replace_re_all -> "String_replace_re_all"
+  | String_extract -> String_extract
+  | String_replace -> String_replace
+  | String_index -> String_index
+  | String_replace_all -> String_replace_all
+  | String_replace_re -> String_replace_re
+  | String_replace_re_all -> String_replace_re_all
 
-let of_relop (relop : Ty.Relop.t) : string =
+let of_relop (relop : Ty.Relop.t) : Feature_map.feat =
   match relop with
-  | Eq -> "Eq"
-  | Ne -> "Ne"
-  | Lt -> "Lt"
-  | LtU -> "LtU"
-  | Le -> "Le"
-  | LeU -> "LeU"
+  | Eq -> Eq
+  | Ne -> Ne
+  | Lt -> Lt
+  | LtU -> LtU
+  | Le -> Le
+  | LeU -> LeU
 
-let of_cvtop (cvtop : Ty.Cvtop.t) : string =
+let of_cvtop (cvtop : Ty.Cvtop.t) : Feature_map.feat =
   match cvtop with
-  | ToString -> "ToString"
-  | OfString -> "OfString"
-  | ToBool -> "ToBool"
-  | OfBool -> "OfBool"
-  | Reinterpret_int -> "Reinterpret_int"
-  | Reinterpret_float -> "Reinterpret_float"
-  | DemoteF64 -> "DemoteF64"
-  | PromoteF32 -> "PromoteF32"
-  | ConvertSI32 -> "ConvertSI32"
-  | ConvertUI32 -> "ConvertUI32"
-  | ConvertSI64 -> "ConvertSI64"
-  | ConvertUI64 -> "ConvertUI64"
-  | TruncSF32 -> "TruncSF32"
-  | TruncUF32 -> "TruncUF32"
-  | TruncSF64 -> "TruncSF64"
-  | TruncUF64 -> "TruncUF64"
-  | Trunc_sat_f32_s -> "Trunc_sat_f32_s"
-  | Trunc_sat_f32_u -> "Trunc_sat_f32_u"
-  | Trunc_sat_f64_s -> "Trunc_sat_f64_s"
-  | Trunc_sat_f64_u -> "Trunc_sat_f64_u"
-  | WrapI64 -> "WrapI64"
-  | Sign_extend _ -> "Sign_extend"
-  | Zero_extend _ -> "Zero_extend"
+  | ToString -> ToString
+  | OfString -> OfString
+  | ToBool -> ToBool
+  | OfBool -> OfBool
+  | Reinterpret_int -> Reinterpret_int
+  | Reinterpret_float -> Reinterpret_float
+  | DemoteF64 -> DemoteF64
+  | PromoteF32 -> PromoteF32
+  | ConvertSI32 -> ConvertSI32
+  | ConvertUI32 -> ConvertUI32
+  | ConvertSI64 -> ConvertSI64
+  | ConvertUI64 -> ConvertUI64
+  | TruncSF32 -> TruncSF32
+  | TruncUF32 -> TruncUF32
+  | TruncSF64 -> TruncSF64
+  | TruncUF64 -> TruncUF64
+  | Trunc_sat_f32_s -> Trunc_sat_f32_s
+  | Trunc_sat_f32_u -> Trunc_sat_f32_u
+  | Trunc_sat_f64_s -> Trunc_sat_f64_s
+  | Trunc_sat_f64_u -> Trunc_sat_f64_u
+  | WrapI64 -> WrapI64
+  | Sign_extend _ -> Sign_extend
+  | Zero_extend _ -> Zero_extend
   (* String *)
-  | String_to_code -> "String_to_code"
-  | String_from_code -> "String_from_code"
-  | String_to_int -> "String_to_int"
-  | String_from_int -> "String_from_int"
-  | String_to_float -> "String_to_float"
-  | String_to_re -> "String_to_re"
+  | String_to_code -> String_to_code
+  | String_from_code -> String_from_code
+  | String_to_int -> String_to_int
+  | String_from_int -> String_from_int
+  | String_to_float -> String_to_float
+  | String_to_re -> String_to_re
 
-let of_ty (ty : Ty.t) : string =
+let of_ty (ty : Ty.t) : Feature_map.feat =
   match ty with
-  | Ty_app -> "Ty_app"
-  | Ty_bitv _ -> "Ty_bitv"
-  | Ty_bool -> "Ty_bool"
-  | Ty_fp _ -> "Ty_fp"
-  | Ty_int -> "Ty_int"
-  | Ty_list -> "Ty_list"
-  | Ty_none -> "Ty_none"
-  | Ty_real -> "Ty_real"
-  | Ty_str -> "Ty_str"
-  | Ty_unit -> "Ty_unit"
-  | Ty_regexp -> "Ty_regexp"
-  | Ty_roundingMode -> "Ty_roundingMode"
+  | Ty_app -> Ty_app
+  | Ty_bitv _ -> Ty_bitv
+  | Ty_bool -> Ty_bool
+  | Ty_fp _ -> Ty_fp
+  | Ty_int -> Ty_int
+  | Ty_list -> Ty_list
+  | Ty_none -> Ty_none
+  | Ty_real -> Ty_real
+  | Ty_str -> Ty_str
+  | Ty_unit -> Ty_unit
+  | Ty_regexp -> Ty_regexp
+  | Ty_roundingMode -> Ty_roundingMode
 
-let of_naryop (naryop : Ty.Naryop.t) : string =
+let of_naryop (naryop : Ty.Naryop.t) : Feature_map.feat =
   match naryop with
-  | Logand -> "Logand"
-  | Logor -> "Logor"
-  | Concat -> "Concat"
-  | Regexp_union -> "Regexp_union"
-  | Distinct -> "Distinct"
+  | Logand -> Logand
+  | Logor -> Logor
+  | Concat -> Concat
+  | Regexp_union -> Regexp_union
+  | Distinct -> Distinct
 
-let of_expr_kind (e : Expr.expr) _ty : string =
+let of_expr_kind (e : Expr.expr) _ty : Feature_map.feat =
   match e with
-  | Val _ -> "Val"
-  | Ptr _ -> "Ptr"
-  | Symbol _ -> "Symbol"
-  | List _ -> "List"
-  | App _ -> "App"
-  | Unop _ -> "Unop"
-  | Binop _ -> "Binop"
-  | Triop _ -> "Triop"
-  | Relop _ -> "Relop"
-  | Cvtop _ -> "Cvtop"
-  | Naryop _ -> "Naryop"
-  | Extract _ -> "Extract"
-  | Concat _ -> "Concat"
-  | Binder _ -> "Binder"
+  | Val _ -> Val
+  | Ptr _ -> Ptr
+  | Symbol _ -> Symbol
+  | List _ -> List_expr
+  | App _ -> App
+  | Unop _ -> Unop_expr
+  | Binop _ -> Binop_expr
+  | Triop _ -> Triop_expr
+  | Relop _ -> Relop_expr
+  | Cvtop _ -> Cvtop_expr
+  | Naryop _ -> Naryop_expr
+  | Extract _ -> Extract
+  | Concat _ -> Concat
+  | Binder _ -> Binder
 
-(* Define all constructors you want to track *)
-let ctor_names =
+let all_feats =
   let expr_kinds =
-    [ "Val"
-    ; "Ptr"
-    ; "Symbol"
-    ; "List"
-    ; "App"
-    ; "Unop"
-    ; "Binop"
-    ; "Triop"
-    ; "Relop"
-    ; "Cvtop"
-    ; "Naryop"
-    ; "Extract"
-    ; "Concat"
-    ; "Binder"
-    ]
+    Feature_map.
+      [ Val
+      ; Ptr
+      ; Symbol
+      ; List_expr
+      ; App
+      ; Unop_expr
+      ; Binop_expr
+      ; Triop_expr
+      ; Relop_expr
+      ; Cvtop_expr
+      ; Naryop_expr
+      ; Extract
+      ; Concat
+      ; Binder
+      ]
   in
   let unops =
     List.map of_unop
@@ -215,7 +215,7 @@ let ctor_names =
   in
   let binops =
     List.map of_binop
-      [ Add
+      [ Ty.Binop.Add
       ; Sub
       ; Mul
       ; Div
@@ -250,7 +250,7 @@ let ctor_names =
   in
   let triops =
     List.map of_triop
-      [ Ite
+      [ Ty.Triop.Ite
       ; List_set (* String *)
       ; String_extract
       ; String_replace
@@ -260,10 +260,10 @@ let ctor_names =
       ; String_replace_re_all
       ]
   in
-  let relops = List.map of_relop [ Eq; Ne; Lt; LtU; Le; LeU ] in
+  let relops = List.map of_relop [ Ty.Relop.Eq; Ne; Lt; LtU; Le; LeU ] in
   let cvtops =
     List.map of_cvtop
-      [ ToString
+      [ Ty.Cvtop.ToString
       ; OfString
       ; ToBool
       ; OfBool
@@ -294,10 +294,12 @@ let ctor_names =
       ; String_to_re
       ]
   in
-  let naryop = List.map of_naryop [ Logand; Logor; Concat; Regexp_union ] in
+  let naryops =
+    List.map of_naryop [ Ty.Naryop.Logand; Logor; Concat; Regexp_union ]
+  in
   let tys =
     List.map of_ty
-      [ Ty_app
+      [ Ty.Ty_app
       ; Ty_bitv 0
       ; Ty_bool
       ; Ty_fp 0
@@ -311,21 +313,17 @@ let ctor_names =
       ; Ty_roundingMode
       ]
   in
-  expr_kinds @ unops @ binops @ triops @ relops @ cvtops @ naryop @ tys
+  Feature_map.[ Max_depth; Mean_depth; Nb_queries; Time ]
+  @ expr_kinds @ unops @ binops @ triops @ relops @ cvtops @ naryops @ tys
 
 let all_feature_names =
-  "solver" :: "model" :: "max_depth" :: "mean_depth" :: "nb_queries" :: "time"
-  :: ctor_names
+  "solver" :: "model" :: List.map Feature_map.feat_to_string all_feats
 
 let feats_to_str solver_name model feats =
-  let row =
-    List.map
-      (fun name ->
-        if String.equal name "solver" then solver_name
-        else if String.equal name "model" then Bool.to_string model
-        else
-          let count = Feature_map.get_feat name feats in
-          string_of_int count )
-      all_feature_names
+  let values =
+    solver_name :: Bool.to_string model
+    :: List.map
+         (fun f -> string_of_int (Feature_map.get_feat f feats))
+         all_feats
   in
-  String.cat (String.concat "," row) "\n"
+  String.cat (String.concat "," values) "\n"
--- src/smtzilla_utils/feature_map.mli
+++ src/smtzilla_utils/feature_map.mli
@@ -1,6 +1,156 @@
-type t
+(* SPDX-License-Identifier: MIT *)
+(* Copyright (C) 2023-2026 formalsec *)
+(* Written by the Smtml programmers *)
 
-type feat = string
+type feat =
+  (* Expr kinds *)
+  | Val
+  | Ptr
+  | Symbol
+  | List_expr
+  | App
+  | Unop_expr
+  | Binop_expr
+  | Triop_expr
+  | Relop_expr
+  | Cvtop_expr
+  | Naryop_expr
+  | Extract
+  | Concat
+  | Binder
+  (* Unops *)
+  | Neg
+  | Not
+  | Clz
+  | Ctz
+  | Popcnt
+  | Abs
+  | Sqrt
+  | Is_normal
+  | Is_subnormal
+  | Is_negative
+  | Is_positive
+  | Is_infinite
+  | Is_nan
+  | Is_zero
+  | Ceil
+  | Floor
+  | Trunc
+  | Nearest
+  | Head
+  | Tail
+  | Reverse
+  | Length
+  | Trim
+  | Regexp_star
+  | Regexp_loop
+  | Regexp_plus
+  | Regexp_opt
+  | Regexp_comp
+  (* Binops *)
+  | Add
+  | Sub
+  | Mul
+  | Div
+  | DivU
+  | Rem
+  | RemU
+  | Shl
+  | ShrA
+  | ShrL
+  | And
+  | Or
+  | Xor
+  | Implies
+  | Pow
+  | Min
+  | Max
+  | Copysign
+  | Rotl
+  | Rotr
+  | At
+  | List_cons
+  | List_append
+  | String_prefix
+  | String_suffix
+  | String_contains
+  | String_last_index
+  | String_in_re
+  | Regexp_range
+  | Regexp_inter
+  | Regexp_diff
+  (* Triops *)
+  | Ite
+  | List_set
+  | String_extract
+  | String_replace
+  | String_index
+  | String_replace_all
+  | String_replace_re
+  | String_replace_re_all
+  (* Relops *)
+  | Eq
+  | Ne
+  | Lt
+  | LtU
+  | Le
+  | LeU
+  (* Cvtops *)
+  | ToString
+  | OfString
+  | ToBool
+  | OfBool
+  | Reinterpret_int
+  | Reinterpret_float
+  | DemoteF64
+  | PromoteF32
+  | ConvertSI32
+  | ConvertUI32
+  | ConvertSI64
+  | ConvertUI64
+  | TruncSF32
+  | TruncUF32
+  | TruncSF64
+  | TruncUF64
+  | Trunc_sat_f32_s
+  | Trunc_sat_f32_u
+  | Trunc_sat_f64_s
+  | Trunc_sat_f64_u
+  | WrapI64
+  | Sign_extend
+  | Zero_extend
+  | String_to_code
+  | String_from_code
+  | String_to_int
+  | String_from_int
+  | String_to_float
+  | String_to_re
+  (* Naryops *)
+  | Logand
+  | Logor
+  | Regexp_union
+  | Distinct
+  (* Types *)
+  | Ty_app
+  | Ty_bitv
+  | Ty_bool
+  | Ty_fp
+  | Ty_int
+  | Ty_list
+  | Ty_none
+  | Ty_real
+  | Ty_str
+  | Ty_unit
+  | Ty_regexp
+  | Ty_roundingMode
+  (* Metadata *)
+  | Depth
+  | Max_depth
+  | Time
+  | Nb_queries
+  | Mean_depth
+
+type t
 
 val empty : t
 
@@ -21,3 +171,7 @@ val add_nb_queries : int -> t -> t
 val add_mean_depth : int -> t -> t
 
 val rename_depth_to_max_depth : t -> t
+
+val feat_to_string : feat -> string
+
+val feat_of_string : string -> feat
