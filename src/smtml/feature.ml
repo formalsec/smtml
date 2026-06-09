e48bd6344269f98c5a430804d1927688d07c093f
Hichem R. A. <hra687261@gmail.com>
2026-06-09 13:40:02 +0200

    Use immutable array for list of all features

--- src/smtml/feature.ml
+++ src/smtml/feature.ml
@@ -162,168 +162,165 @@ let of_expr_kind (e : Expr.expr) _ty : Feature_map.feat =
   | Concat _ -> Concat
   | Binder _ -> Binder
 
-let all_feats =
-  let expr_kinds =
-    Feature_map.
-      [ Val
-      ; Ptr
-      ; Symbol
-      ; List_expr
-      ; App
-      ; Unop_expr
-      ; Binop_expr
-      ; Triop_expr
-      ; Relop_expr
-      ; Cvtop_expr
-      ; Naryop_expr
-      ; Extract
-      ; Concat
-      ; Binder
-      ]
-  in
-  let unops =
-    List.map of_unop
-      [ Ty.Unop.Neg
-      ; Not
-      ; Clz
-      ; Ctz
-      ; Popcnt
-      ; Abs (* Float *)
-      ; Sqrt
-      ; Is_normal
-      ; Is_subnormal
-      ; Is_negative
-      ; Is_positive
-      ; Is_infinite
-      ; Is_nan
-      ; Is_zero
-      ; Ceil
-      ; Floor
-      ; Trunc
-      ; Nearest
-      ; Head
-      ; Tail
-      ; Reverse
-      ; Length
-      ; Trim (* String *)
-      ; Regexp_star (* RegExp *)
-      ; Regexp_loop (0, 0)
-      ; Regexp_plus
-      ; Regexp_opt
-      ; Regexp_comp
-      ]
-  in
-  let binops =
-    List.map of_binop
-      [ Ty.Binop.Add
-      ; Sub
-      ; Mul
-      ; Div
-      ; DivU
-      ; Rem
-      ; RemU
-      ; Shl
-      ; ShrA
-      ; ShrL
-      ; And
-      ; Or
-      ; Xor
-      ; Implies
-      ; Pow
-      ; Min
-      ; Max
-      ; Copysign
-      ; Rotl
-      ; Rotr
-      ; At
-      ; List_cons
-      ; List_append (* String *)
-      ; String_prefix
-      ; String_suffix
-      ; String_contains
-      ; String_last_index
-      ; String_in_re (* Regexp *)
-      ; Regexp_range
-      ; Regexp_inter
-      ; Regexp_diff
-      ]
-  in
-  let triops =
-    List.map of_triop
-      [ Ty.Triop.Ite
-      ; List_set (* String *)
-      ; String_extract
-      ; String_replace
-      ; String_index
-      ; String_replace_all
-      ; String_replace_re
-      ; String_replace_re_all
-      ]
-  in
-  let relops = List.map of_relop [ Ty.Relop.Eq; Ne; Lt; LtU; Le; LeU ] in
-  let cvtops =
-    List.map of_cvtop
-      [ Ty.Cvtop.ToString
-      ; OfString
-      ; ToBool
-      ; OfBool
-      ; Reinterpret_int
-      ; Reinterpret_float
-      ; DemoteF64
-      ; PromoteF32
-      ; ConvertSI32
-      ; ConvertUI32
-      ; ConvertSI64
-      ; ConvertUI64
-      ; TruncSF32
-      ; TruncUF32
-      ; TruncSF64
-      ; TruncUF64
-      ; Trunc_sat_f32_s
-      ; Trunc_sat_f32_u
-      ; Trunc_sat_f64_s
-      ; Trunc_sat_f64_u
-      ; WrapI64
-      ; Sign_extend 0
-      ; Zero_extend 0 (* String *)
-      ; String_to_code
-      ; String_from_code
-      ; String_to_int
-      ; String_from_int
-      ; String_to_float
-      ; String_to_re
-      ]
-  in
-  let naryops =
-    List.map of_naryop [ Ty.Naryop.Logand; Logor; Concat; Regexp_union ]
-  in
-  let tys =
-    List.map of_ty
-      [ Ty.Ty_app
-      ; Ty_bitv 0
-      ; Ty_bool
-      ; Ty_fp 0
-      ; Ty_int
-      ; Ty_list
-      ; Ty_none
-      ; Ty_real
-      ; Ty_str
-      ; Ty_unit
-      ; Ty_regexp
-      ; Ty_roundingMode
-      ]
-  in
-  Feature_map.[ Max_depth; Mean_depth; Nb_queries; Time ]
-  @ expr_kinds @ unops @ binops @ triops @ relops @ cvtops @ naryops @ tys
+let all_feats : Feature_map.feat Iarray.t =
+  Feature_map.
+    [| (* Metadata *)
+       Max_depth
+     ; Mean_depth
+     ; Nb_queries
+     ; Time
+     ; (* Expr kinds *)
+       Val
+     ; Ptr
+     ; Symbol
+     ; List_expr
+     ; App
+     ; Unop_expr
+     ; Binop_expr
+     ; Triop_expr
+     ; Relop_expr
+     ; Cvtop_expr
+     ; Naryop_expr
+     ; Extract
+     ; Concat
+     ; Binder
+     ; (* Unops *)
+       Neg
+     ; Not
+     ; Clz
+     ; Ctz
+     ; Popcnt
+     ; Abs
+     ; Sqrt
+     ; Is_normal
+     ; Is_subnormal
+     ; Is_negative
+     ; Is_positive
+     ; Is_infinite
+     ; Is_nan
+     ; Is_zero
+     ; Ceil
+     ; Floor
+     ; Trunc
+     ; Nearest
+     ; Head
+     ; Tail
+     ; Reverse
+     ; Length
+     ; Trim
+     ; Regexp_star
+     ; Regexp_loop
+     ; Regexp_plus
+     ; Regexp_opt
+     ; Regexp_comp
+     ; (* Binops *)
+       Add
+     ; Sub
+     ; Mul
+     ; Div
+     ; DivU
+     ; Rem
+     ; RemU
+     ; Shl
+     ; ShrA
+     ; ShrL
+     ; And
+     ; Or
+     ; Xor
+     ; Implies
+     ; Pow
+     ; Min
+     ; Max
+     ; Copysign
+     ; Rotl
+     ; Rotr
+     ; At
+     ; List_cons
+     ; List_append
+     ; String_prefix
+     ; String_suffix
+     ; String_contains
+     ; String_last_index
+     ; String_in_re
+     ; Regexp_range
+     ; Regexp_inter
+     ; Regexp_diff
+     ; (* Triops *)
+       Ite
+     ; List_set
+     ; String_extract
+     ; String_replace
+     ; String_index
+     ; String_replace_all
+     ; String_replace_re
+     ; String_replace_re_all
+     ; (* Relops *)
+       Eq
+     ; Ne
+     ; Lt
+     ; LtU
+     ; Le
+     ; LeU
+     ; (* Cvtops *)
+       ToString
+     ; OfString
+     ; ToBool
+     ; OfBool
+     ; Reinterpret_int
+     ; Reinterpret_float
+     ; DemoteF64
+     ; PromoteF32
+     ; ConvertSI32
+     ; ConvertUI32
+     ; ConvertSI64
+     ; ConvertUI64
+     ; TruncSF32
+     ; TruncUF32
+     ; TruncSF64
+     ; TruncUF64
+     ; Trunc_sat_f32_s
+     ; Trunc_sat_f32_u
+     ; Trunc_sat_f64_s
+     ; Trunc_sat_f64_u
+     ; WrapI64
+     ; Sign_extend
+     ; Zero_extend
+     ; String_to_code
+     ; String_from_code
+     ; String_to_int
+     ; String_from_int
+     ; String_to_float
+     ; String_to_re
+     ; (* Naryops *)
+       Logand
+     ; Logor
+     ; Regexp_union
+     ; (* Types *)
+       Ty_app
+     ; Ty_bitv
+     ; Ty_bool
+     ; Ty_fp
+     ; Ty_int
+     ; Ty_list
+     ; Ty_none
+     ; Ty_real
+     ; Ty_str
+     ; Ty_unit
+     ; Ty_regexp
+     ; Ty_roundingMode
+    |]
 
 let all_feature_names =
-  "solver" :: "model" :: List.map Feature_map.feat_to_string all_feats
+  "solver" :: "model"
+  :: Iarray.fold_right
+       (fun feat acc -> Feature_map.feat_to_string feat :: acc)
+       all_feats []
 
 let feats_to_str solver_name model feats =
   let values =
-    solver_name :: Bool.to_string model
-    :: List.map
-         (fun f -> string_of_int (Feature_map.get_feat f feats))
-         all_feats
+    solver_name :: Bool.to_string model :: "solver" :: "model"
+    :: Iarray.fold_right
+         (fun feat acc -> string_of_int (Feature_map.get_feat feat feats) :: acc)
+         all_feats []
   in
   String.cat (String.concat "," values) "\n"
